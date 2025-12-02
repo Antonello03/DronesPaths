a262f388513f4be5c30ad942229e055faef74954
Tuo Nome <tuamail@example.com>
2025-12-02 13:15:27 +0100

    Initial Repository Setup

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+
+def checkSolutionFeasibility(solution):
+    """
+    Checks if the given solution is feasible.
+    """
+    pass
+
+
+def visualizeSolution(solution) -> None:
+    """
+    Generate a 3D plot to visualize the given drone path.
+    """
+
+    #TODO
+
+    pass
